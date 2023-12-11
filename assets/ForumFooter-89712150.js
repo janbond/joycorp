1cd8280867407ca7f2bb9552e797da31ac1b5492
janbond <was760910@gmail.com>
2023-12-11 10:37:29 +0800

    重新 build 一次

---
+++ assets/ForumFooter-89712150.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as s,p as c,g as _,a as r}from"./index-c361cbbc.js";const a={data(){return{}},components:{}},n=e=>(c("data-v-5ee67263"),e=e(),_(),e),p=n(()=>r("p",null,"網石棒辣椒股份有限公司版權所有 © Netmarble Joybomb Inc. All rights reserved.",-1)),d=[p];function l(e,u,i,m,f,h){return t(),s("footer",null,d)}const F=o(a,[["render",l],["__scopeId","data-v-5ee67263"]]);export{F};
